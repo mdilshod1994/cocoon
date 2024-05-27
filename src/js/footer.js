a1435913d1eb26e711d7845482097bf7ac3ee838
mdilshod1994 <mdilshod1994@gmail.com>
2024-05-27 19:27:21 +0500

    поправка стилей в моб версии и прикрепление ссылок

--- src/js/footer.js
+++ src/js/footer.js
@@ -4,3 +4,10 @@ $(".footer__menu a").on("click", function (e) {
 	jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 600);
 	return false;
 });
+const copyrightsYear = document.querySelectorAll('#cr-year')
+if(copyrightsYear){
+  const currYear = new Date().getFullYear()
+  copyrightsYear.forEach((cr)=>{
+    cr.innerHTML = currYear
+  })
+}
--- src/js/future.js
+++ src/js/future.js
@@ -1,3 +1,3 @@
-$(".future__top-box").on("click", function (e) {
-	$(".future__top").addClass('hidden');
-});
+// $(".future__top-box").on("click", function (e) {
+// 	$(".future__top").addClass('hidden');
+// });
