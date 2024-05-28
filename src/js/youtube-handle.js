c1008cb3c4b3f3c3089ae4ad81f85e5af796bbcb
mdilshod1994 <mdilshod1994@gmail.com>
2024-05-28 16:55:25 +0500

    добавил видео с ютуба в модалке

--- src/js/youtube-handle.js
+++ src/js/youtube-handle.js
@@ -1,20 +1,31 @@
-// const player= document.getElementById('c-player')
-// const tag = document.createElement("script")
-// tag.src = 'https://www.youtube.com/iframe_api'
-// let firstScriptTag = document.getElementsByTagName('script')[0]
-// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
-
-// function onYouTubeIframeAPIReady(){
-//   let ytPlayer;
-//   ytPlayer = new YT.Player(player, {
-//     videoId: 'jkPVT55GAOo',
-//     playerVars: { 'autoplay': 1, 'controls': 0, loop:1 , autohide: 1},
-//     events: {
-//       'onReady': onPlayerReady,
-//     }
-//   })
-// }
-// function onPlayerReady(event) {
-//   event.target.playVideo();
-//   console.log(event);
-// }
+const YTModal= document.getElementById('yt-modal')
+const body= document.querySelector('body')
+const YToverlay = document.getElementById("yt-overlay")
+const YTcloseBtn = document.getElementById('yt-close')
+const YTopenBtn = document.getElementById('video-btn')
+var iframe = YTModal.getElementsByTagName('iframe')[0].contentWindow;
+if(YTModal){
+  YTopenBtn.addEventListener('click', ()=>{
+    body.classList.add('lock')
+    YTModal.classList.add('active')
+    console.log(iframe);
+    iframe.postMessage(
+      '{"event":"command","func":"' + "playVideo" + '","args":""}',
+      "*"
+    );
+  })
+  YToverlay.addEventListener('click', ()=>{
+    closeModal()
+  })
+  YTcloseBtn.addEventListener('click', ()=>{
+    closeModal()
+  })
+  function closeModal(){
+    YTModal.classList.remove('active')
+    body.classList.remove('lock')
+    iframe.postMessage(
+      '{"event":"command","func":"' + "stopVideo" + '","args":""}',
+      "*"
+    );
+  }
+}
