6b851c64be6b65e41661a4750c7da33faecbe72e
mdilshod1994 <mdilshod1994@gmail.com>
2024-05-28 11:34:59 +0500

    добавил видео с ютуба, бэкгроунд с лупом

---
+++ src/js/youtube-handle.js
@@ -0,0 +1,20 @@
+// const player= document.getElementById('c-player')
+// const tag = document.createElement("script")
+// tag.src = 'https://www.youtube.com/iframe_api'
+// let firstScriptTag = document.getElementsByTagName('script')[0]
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+
+// function onYouTubeIframeAPIReady(){
+//   let ytPlayer;
+//   ytPlayer = new YT.Player(player, {
+//     videoId: 'jkPVT55GAOo',
+//     playerVars: { 'autoplay': 1, 'controls': 0, loop:1 , autohide: 1},
+//     events: {
+//       'onReady': onPlayerReady,
+//     }
+//   })
+// }
+// function onPlayerReady(event) {
+//   event.target.playVideo();
+//   console.log(event);
+// }
